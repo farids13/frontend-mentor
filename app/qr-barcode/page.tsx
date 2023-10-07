a1f3cdc3fc98e3337815840f70992d3aaee865ba
farids13 <faridsatria24@gmail.com>
2023-10-07 21:32:24 +0700

    Improve More Style And Fixing Miss Design

--- app/qr-barcode/page.tsx
+++ app/qr-barcode/page.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import imgBarcode from "../../public/qr/images/image-qr-code.png";
 import Image from 'next/image';
+import imgBgDesign from "../../public/qr/design/desktop-design.jpg"
+import { url } from 'inspector';
 
 const qrBarcode = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-sky-200">
-      <main className="container mx-auto w-[20em] h-[31em] rounded-3xl p-4 bg-white shadow-lg shadow-gray-600">
+    <>
+    <div className="flex justify-center items-center h-screen bg-lightGray bg-example-bg bg-no-repeat bg-fixed bg-center" >
+      <main className="container font-outfit mx-auto w-[20em] h-[31em] rounded-3xl p-4 bg-white shadow-lg shadow-gray-600">
         <Image src={imgBarcode} alt="Logo Barcode" className='rounded-xl'/>
-        <div className="title mt-4 p-2 text-center text-xl leading-3">
-          <h1 className="text-center font-bold text-xl tracking-wide mb-4">Improve your front-end skills by building projects</h1>
-          <span className="text-sm items-center">Scan the QR code to visit Frontend Mentor and take your coding skills to <br /> the next level</span>
+        <div className="title mt-4 p-2 text-center text-base leading-3">
+          <h1 className="text-center font-bold text-xl tracking-wider mb-3">Improve your front-end skills by building projects</h1>
+          <span className="text-base items-center tracking-tight leading-5">Scan the QR code to visit Frontend Mentor and take your coding skills to <br /> the next level</span>
         </div>
       </main>
-  </div>
+    </div>
+  </>
   )
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+     
+    },
+    colors :{
+      "white": "hsl(0, 0%, 100%)",
+      "lightGray": "hsl(212, 45%, 89%)",
+      "grayishBlue": "hsl(220, 15%, 55%)",
+      "darkBlue": "hsl(218, 44%, 22%)"    
+    },
+    fontFamily : {
+      'outfit': ['Outfit', 'sans'],
+    },
+    backgroundImage: {
+      'example-bg' : "url('/qr/design/desktop-design.jpg')",
     },
   },
   plugins: [],
