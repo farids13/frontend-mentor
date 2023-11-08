4e06aa229aff1f5338f3099e367a1ba91acf0155
farids13 <faridsatria24@gmail.com>
2023-11-08 15:59:56 +0700

    comment auth function

--- lib/auth.ts
+++ lib/auth.ts
@@ -81,15 +81,15 @@ export const authConfig : NextAuthOptions = {
    
 };
 
-export async function loginIsRequiredServer() {
-    const session = await getServerSession(authConfig);
-    if (!session) return redirect("/");
-}
+// export async function loginIsRequiredServer() {
+//     const session = await getServerSession(authConfig);
+//     if (!session) return redirect("/");
+// }
 
-export function loginIsRequiredClient() {
-    if (typeof window !== "undefined") {
-    const session = useSession();
-    const router = useRouter();
-    if (!session) router.push("/");
-    }
-}
+// export function loginIsRequiredClient() {
+//     if (typeof window !== "undefined") {
+//     const session = useSession();
+//     const router = useRouter();
+//     if (!session) router.push("/");
+//     }
+// }
